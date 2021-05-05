20a7b790bd9cba7a873a8928e2e9fd87235d6ac5
2023RafaDias <2023rafaeladias@gmail.com>
2021-05-05 17:04:03 -0300

    create patternChallenge.py

---
+++ patternChallenge.py
@@ -0,0 +1,26 @@
+again = ""
+while again != "n":
+    size = int(input("What will be the size of your grid?"))
+    array = []
+    for i in range(0,size):
+        array.append([])
+        for i in range(0,size):
+            array[len(array)-1].append(0)
+
+    for i in range(0,size):
+        for ii in range(0,size):
+            if i-1 >= 0:
+                array[i][ii] += 1
+            if i+1 < size:
+                array[i][ii] += 1
+            if ii-1 >= 0:
+                array[i][ii] += 1
+            if ii+1 < size:
+                array[i][ii] += 1
+
+    for i in range(0,size):
+        for ii in range(0,size):
+            print(array[i][ii], end=" ")
+        print("")
+    
+    again = input("Would you like to go again?")
