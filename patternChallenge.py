33263f6d35d7adfeb85fbc09a60b5511c82bc397
2023RafaDias <2023rafaeladias@gmail.com>
2021-05-12 00:43:02 -0300

    accepts different widths and heights

--- patternChallenge.py
+++ patternChallenge.py
@@ -3,31 +3,40 @@ while again != "n":
     error = "yes"
     while error == "yes":
         try:
-            size = int(input("What will be the size of your grid? "))
+            size = int(input("What will be the width of your grid? "))
+            error = "no"
+        except ValueError:
+            print("Integer Only!")
+        try:
+            size2 = int(input("What will be the height of your grid? "))
             error = "no"
         except ValueError:
             print("Integer Only!")
             
     array = []
-    for i in range(0,size):
+    for i in range(0,size2):
         array.append([])
         for i in range(0,size):
             array[len(array)-1].append(0)
 
-    for i in range(0,size):
+    for i in range(0,size2):
         for ii in range(0,size):
             if i-1 >= 0:
                 array[i][ii] += 1
-            if i+1 < size:
+            if i+1 < size2:
                 array[i][ii] += 1
             if ii-1 >= 0:
                 array[i][ii] += 1
             if ii+1 < size:
                 array[i][ii] += 1
 
-    for i in range(0,size):
-        for ii in range(0,size):
-            print(array[i][ii], end=" ")
-        print("")
+    if size == 0 or size2 == 0:
+        print(array)
+    else:
+        for i in range(0,size2):
+            for ii in range(0,size):
+                print(array[i][ii], end=" ")
+            print("")
+    
     
     again = input("Would you like to go again? y/n ")
