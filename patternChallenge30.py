54713820a75e0540f25a8788fc08ed17d807d0e6
2023RafaDias <2023rafaeladias@gmail.com>
2021-05-12 01:03:21 -0300

    Create patternChallenge30.py

---
+++ patternChallenge30.py
@@ -0,0 +1,23 @@
+again = "y"
+while again == "y":
+    n = int(input("Enter number here: "))
+    n += 1
+    count = 1
+    count2 = 1
+    while count2 != n:
+        for i in range (1,count2+1):
+            print(count, end=" ")
+            count += 1
+        print("")
+        count2 += 1
+
+    count2 -= 1
+    while count2 != 0:
+        count2 -= 1
+        for i in range (1,count2+1):
+            print(count, end=" ")
+            count += 1
+
+    again = input("Again? y/n")
+    
+    
